15bbf5128ac2b8db82e12e03e1a395fec097a706
petrborr <petrborr@gmail.com>
2022-12-09 16:54:06 +0200

    fix: title tag changes 3 sec after window loads

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  const title = document.querySelector("title")
+  setTimeout(() => {
+    title.innerHTML = "One new message"
+  }, "3000")
+  
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
